caaae8d3911abe96bdb2fd3368abf99f256f3324
tellmewyatt <wyatt_cannon@sbcglobal.net>
2021-02-19 13:37:04 -0800

    Set up serverHost so only host can change times, no one else. Also renamed server_v6.js to server.js

--- files/settingScript.js
+++ files/settingScript.js
@@ -67,6 +67,17 @@ wWorker.onmessage = function(e){
 }
 // "Enter" to start playback "Esc" to stop, play is playback var
 var play = false;
+document.addEventListener('keydown', function(event) {
+    console.log(event.keyCode);
+	if(event.keyCode==13){
+		socket.emit('message', "strtstp,"+ts.now().toString());
+		//play = true;
+	}
+	if(event.keyCode==27){
+		//play = false;
+		socket.emit('message', "strtstp,stop");
+	}
+});
 // FOR CLEFS: q = treble, l = bass, n = alto 
 // Load Canvas and Context
 function setUpMIDI(){
@@ -794,5 +805,18 @@ function drawCountDown(num){
 				Parts[p].writeMIDI();
 			}
 		}
+	}
+}
+function setTime(){
+	var clickX = event.clientX*2;
+	var clickY = event.clientY*2
+	if(clickY < scrheight/10){
+		CompClock = Math.round(clickX/scrwidth*totalTime);
+		for(i in Parts){
+			Parts[i].checkClefs();
+			Parts[i].nextNotes = [];
+			Parts[i].nextNoteTimes = [];
+		}
+		socket.emit('message', "CompClock,"+CompClock.toString());
 	}
 }
